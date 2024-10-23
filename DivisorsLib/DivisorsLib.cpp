966adfb6e795e6b794280be02480df1dd3870245
SeanC2003 <chisvt1513176_aehit@students.vizja.pl>
2024-10-24 00:31:52 +0200

    new algorithm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,29 @@
 #include <vector>
-#include "Divisors.h"
+#include <cmath>
+#include <algorithm>  
+
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> small_divisors;  
+    std::vector<unsigned long long> large_divisors;  
+    unsigned long long sqrt_n = std::sqrt(number);   
+
+    for (unsigned long long i = 1; i <= sqrt_n; ++i)
+    {
+        if (number % i == 0)
+        {
+            small_divisors.push_back(i);             
+            if (i != number / i)
+            {
+                large_divisors.push_back(number / i); 
+            }
+        }
+    }
+
+    
+    small_divisors.insert(small_divisors.end(), large_divisors.rbegin(), large_divisors.rend());
+
+    return small_divisors;  
 }
+
